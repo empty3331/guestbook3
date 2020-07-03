4074f8312199af9be048eed75dc538d713d11703
'Ju-Hye' <'empty3331@naver.com'>
2020-07-03 11:30:39 +0900

    스프링으로 만든 방명록

---
+++ src/main/java/com/javaex/controller/GuestBookController.java
@@ -0,0 +1,65 @@
+package com.javaex.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.dao.GuestBookDao;
+import com.javaex.vo.GuestBookVo;
+
+@Controller
+@RequestMapping("/guest")
+public class GuestBookController {
+	
+	@RequestMapping("/addlist" )
+	public String addList(Model model) {
+		System.out.println("addlist");
+		
+		GuestBookDao gbDao = new GuestBookDao();
+		List<GuestBookVo> gbList = gbDao.getGuestBookList();
+		
+		
+		model.addAttribute("gbList",gbList);
+		
+		return "/WEB-INF/view/addList.jsp";
+	}
+	
+	@RequestMapping("/add" )
+	public String add(@ModelAttribute GuestBookVo gbVo) {
+		System.out.println("add");
+		
+		GuestBookDao gbDao = new GuestBookDao();
+		gbDao.guestBookInsert(gbVo);
+		
+		System.out.println(gbVo.toString());
+		
+		return "redirect:/guest/addlist";
+	}
+	
+	@RequestMapping("/deleteForm" )
+	public String deleteForm(Model model,@RequestParam("no") int no) {
+		System.out.println("deleteForm");
+	
+		model.addAttribute("no", no);
+	
+		return "/WEB-INF/view/deleteForm.jsp";
+	}
+	
+	@RequestMapping("/delete" )
+	public String delete(@RequestParam("no") int no,
+						 @RequestParam("password") String password) {
+		
+		GuestBookDao gbDao = new GuestBookDao();
+		gbDao.guestBookDelete(no, password);
+		
+		return "redirect:/guest/addlist";
+				
+	}
+	
+	
+
+}
---
+++ webapp/WEB-INF/view/addList.jsp
@@ -0,0 +1,52 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+
+<form action = "/guestbook3/guest/add" method = "post">
+	<table border="1">
+		<tr>
+			<td>이름</td>
+			<td><input type="text" name="name"></td>
+			<td>비밀번호</td>
+			<td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+		 <td colspan="4"><textarea style="width:100%;" name = "content"></textarea></td>
+		</tr>
+		<tr>
+		 
+		 <td colspan="4">		 
+		 <button type="submit">등록</button></td>
+		</tr>
+	</table>
+	
+  </form>
+  <br>
+    
+    <!-- for(GuestBookVo guest : gbList){ -->
+   <c:forEach items="${gbList }" var="gbList" >
+	<table border = "1" style = "width:500px;">
+		<tr>
+			<td style = "width:50px;">${gbList.no }</td>
+			<td style = "width:200px;">${gbList.name }</td>
+			<td style = "width:200px;">${gbList.date }</td>
+			<td style = "width:50px;"> <a href = "/guestbook3/guest/deleteForm?no=${gbList.no }">삭제</a> </td>
+		</tr>
+		<tr>
+			<td colspan="4">${gbList.content }</td>
+		</tr>
+	</table>
+	<br>
+	</c:forEach>
+
+</body>
+</html>
